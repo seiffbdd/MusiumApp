9b1e7cb5ad1d5b74b0d9e66e3c1beb872fc0d11c
Seif Elnawawy <elnawawyseif@gmail.com>
2025-06-21 02:06:49 +0300

    added flutter project code

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart';
+
+/// a class that contains the shared colors in the app
+abstract class AppColors {
+  static const Color primaryBackgroundColor = Color(0XFF121111);
+  static const Color caribbeanBlueColor = Color(0XFF06A0B5);
+  static const Color turquoiseBlueColor = Color(0XFF00C2CB);
+  static const Color skyBlueColor = Color(0XFF39C0D4);
+  static const Color whiteColor = Color(0XFFFFFFFF);
+  static const Color offWhiteColor = Color(0XFFDBE7E8);
+  static const Color charcoalColor = Color(0XFF1E1E1E);
+}
