9515edc96754a6a77d7ed4d54f3a9ac19db1bad4
Seif Elnawawy <elnawawyseif@gmail.com>
2025-06-23 03:57:58 +0300

    added firebase for the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:musium/config/routes/app_router.dart';
 import 'package:musium/config/theme/app_theme.dart';
 import 'package:musium/core/utils/service_locator.dart';
+import 'package:musium/firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setup();
   runApp(const MyApp());
 }
